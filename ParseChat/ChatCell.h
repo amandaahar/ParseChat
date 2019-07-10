02069596dfe18d52f939cd74a4c1a2da54fb7a98
Amanda Harris <amandahar@fb.com>
2019-07-10 11:51:41 -0700

    initial commit

---
+++ ParseChat/ChatCell.h
@@ -0,0 +1,17 @@
+//
+//  ChatCell.h
+//  ParseChat
+//
+//  Created by amandahar on 7/10/19.
+//  Copyright © 2019 amandahar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
